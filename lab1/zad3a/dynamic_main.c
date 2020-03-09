1a66aa2644c4b08d534e06b4b54df53de3445a57
Qizot <perzylo.jakub@gmial.com>
2020-03-09 18:08:32 +0100

    Update changed all programs to use cmd instead of stdio

--- lab1/zad3a/dynamic_main.c
+++ lab1/zad3a/dynamic_main.c
@@ -9,6 +9,11 @@
 #include <time.h>
 #include <zconf.h>
 
+#define CHECK_ARGC(N) \
+    if (N >= argc) { \
+        perror("Ivalid number of arguments"); \
+        exit(0); \
+    }\
 
 double elapsed_time(clock_t start, clock_t end) {
     return (double) (end - start) / sysconf(_SC_CLK_TCK);
@@ -25,6 +30,17 @@ void print_time(char* header, clock_t real[2], struct tms* tms_time) {
     printf("\n\n");
 }
 
+
+const char* const commands[] = {"create_table", "compare_pairs", "remove_operation", "remove_block"};
+
+int is_command(char* string) {
+    for (int i = 0; i < 4; i++) {
+        if (strcmp(commands[i], string) == 0)
+            return 1;
+    }
+    return 0;
+}
+
 void *dl_handle;
 typedef void *(*func_handler)();
 
@@ -36,89 +52,79 @@ int main(int argc, char** argv) {
         return 0;
     }
 
+    func_handler _create_block_manager = dlsym(dl_handle, "create_block_manager");
+    func_handler _add_edit_block = dlsym(dl_handle, "add_edit_block");
+    func_handler _create_diff_file = dlsym(dl_handle, "create_diff_file");
+    func_handler _delete_operation = dlsym(dl_handle, "delete_operation");
+    func_handler _delete_edit_block = dlsym(dl_handle, "delete_edit_block");
+
+
     struct tms tms_time[2];
     clock_t real_time[2];
 
     struct edit_block_manager* manager = NULL;
-    char* line;
-    size_t len = 0;
-    while(getline(&line, &len, stdin) != -1) {
-        if (strstr(line, "create_table")) {
-            strtok(line, " ");
-            int blocks = atoi(strtok(NULL, " "));
-            if (manager != NULL) {
-                func_handler fdelete_manager;
-                *(void **) (&fdelete_manager) = dlsym(dl_handle, "delete_manager");
-                fdelete_manager(manager);
-                manager = NULL;
-            }
+    int i = 1;
+    while (i < argc) {
+        if (strcmp(argv[i], "create_table") == 0) {
+            i++;
+            CHECK_ARGC(i);
 
+            int blocks = atoi(argv[i]);
+            
             real_time[0] = times(&tms_time[0]);
-            func_handler fcreate_block_manager;
-            *(void **) (&fcreate_block_manager) = dlsym(dl_handle, "create_block_manager");
-            manager = fcreate_block_manager(blocks);
+            manager = _create_block_manager(blocks);
             real_time[1] = times(&tms_time[1]);
 
             print_time("create_table", real_time, tms_time);
-
+            i++;
         }
-
-        if (strstr(line, "compare_pairs")) {
-            strtok(line, " ");
-            char* file;
-            int len = 0;
-            char** files = calloc(1000, sizeof(char*));
-
-            while((file = strtok(NULL, " "))) {
-                files[len] = file;
-                len += 1;
-            }
-            func_handler fcreate_diff_file;
-            *(void **) (&fcreate_diff_file) = dlsym(dl_handle, "create_diff_file");
-            func_handler fadd_edit_block;
-            *(void **) (&fadd_edit_block) = dlsym(dl_handle, "add_edit_block");
+        else if (strcmp(argv[i], "compare_pairs") == 0) {
+            i++;
+            CHECK_ARGC(i);
 
             real_time[0] = times(&tms_time[0]);
-            for (int i = 0; i < len; i++) {
-                char* firstfile = strtok(files[i], ":");
-                char* secondfile = strtok(NULL, "\n");
-                char* tmp_file = fcreate_diff_file(firstfile, secondfile);
-                fadd_edit_block(manager, tmp_file);
-            }
 
-            free(files);
+            while (i < argc && !is_command(argv[i])) {
+                char* firstfile = strtok(argv[i], ":");
+                char* secondfile = strtok(NULL, "\n");
+                char* tmp_file = _create_diff_file(firstfile, secondfile);
+                _add_edit_block(manager, tmp_file);
+                i++;
+            }
 
             real_time[1] = times(&tms_time[1]);
+            
             print_time("add_edit_block", real_time, tms_time);
         }
-
-        if (strstr(line, "remove_block")) {
-            strtok(line, " ");
-            int idx = atoi(strtok(NULL, " "));
-
+        else if (strcmp(argv[i], "remove_block") == 0) {
+            i++;
+            CHECK_ARGC(i);  
+            int idx = atoi(argv[i]);
             real_time[0] = times(&tms_time[0]);
-            func_handler fdelete_edit_block;
-            *(void **) (&fdelete_edit_block) = dlsym(dl_handle, "delete_edit_block");
-            fdelete_edit_block(manager, idx);
+            _delete_edit_block(manager, idx);
             real_time[1] = times(&tms_time[1]);
 
             print_time("delete_edit_block", real_time, tms_time);
+
+            i++;
         }
-
-        if (strstr(line, "remove_operation")) {
-            strtok(line, " ");
-            int block_idx = atoi(strtok(NULL, " "));
-            int op_idx = atoi(strtok(NULL, " "));
-
+        else if (strcmp(argv[i], "remove_operation") == 0) {
+            i += 2;
+            CHECK_ARGC(i);
+            
+            int block_idx = atoi(argv[i-1]);
+            int op_idx = atoi(argv[i]);
+            
             real_time[0] = times(&tms_time[0]);
-            func_handler fdelete_operation;
-            *(void **) (&fdelete_operation) = dlsym(dl_handle, "delete_operation");
-            fdelete_operation(manager, block_idx, op_idx);
+            _delete_operation(manager, block_idx, op_idx);
             real_time[1] = times(&tms_time[1]);
 
+            
             print_time("delete_operation", real_time, tms_time);
+            i++;
+        } else {
+            i++;
         }
     }
-
     return 0;
 }
--- lab1/zad3a/main.c
+++ lab1/zad3a/main.c
@@ -7,6 +7,12 @@
 #include <time.h>
 #include <zconf.h>
 
+#define CHECK_ARGC(N) \
+    if (N >= argc) { \
+        perror("Ivalid number of arguments"); \
+        exit(0); \
+    }\
+
 double elapsed_time(clock_t start, clock_t end) {
     return (double) (end - start) / sysconf(_SC_CLK_TCK);
 }
@@ -23,6 +29,15 @@ void print_time(char* header, clock_t real[2], struct tms* tms_time) {
     printf("\n\n");
 }
 
+const char* const commands[] = {"create_table", "compare_pairs", "remove_operation", "remove_block"};
+
+int is_command(char* string) {
+    for (int i = 0; i < 4; i++) {
+        if (strcmp(commands[i], string) == 0)
+            return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char** argv) {
 
@@ -30,74 +45,69 @@ int main(int argc, char** argv) {
     clock_t real_time[2];
 
     struct edit_block_manager* manager = NULL;
-    char* line;
-    size_t len = 0;
-    while(getline(&line, &len, stdin) != -1) {
-        if (strstr(line, "create_table")) {
-            strtok(line, " ");
-            int blocks = atoi(strtok(NULL, " "));
-            if (manager != NULL) {
-                delete_manager(manager);
-                manager = NULL;
-            }
+    int i = 1;
+    while (i < argc) {
+        if (strcmp(argv[i], "create_table") == 0) {
+            i++;
+            CHECK_ARGC(i);
 
+            int blocks = atoi(argv[i]);
+            
             real_time[0] = times(&tms_time[0]);
             manager = create_block_manager(blocks);
             real_time[1] = times(&tms_time[1]);
 
             print_time("create_table", real_time, tms_time);
-
+            i++;
         }
-
-        if (strstr(line, "compare_pairs")) {
-            strtok(line, " ");
-            char* file;
-            int len = 0;
-            char** files = calloc(1000, sizeof(char*));
-
-            while((file = strtok(NULL, " "))) {
-                files[len] = file;
-                len += 1;
-            }
+        else if (strcmp(argv[i], "compare_pairs") == 0) {
+            i++;
+            CHECK_ARGC(i);
 
             real_time[0] = times(&tms_time[0]);
-            for (int i = 0; i < len; i++) {
-                char* firstfile = strtok(files[i], ":");
+
+            while (i < argc && !is_command(argv[i])) {
+                char* firstfile = strtok(argv[i], ":");
                 char* secondfile = strtok(NULL, "\n");
                 char* tmp_file = create_diff_file(firstfile, secondfile);
                 add_edit_block(manager, tmp_file);
+                i++;
             }
 
-            free(files);
-
             real_time[1] = times(&tms_time[1]);
+            
             print_time("add_edit_block", real_time, tms_time);
         }
-
-        if (strstr(line, "remove_block")) {
-            strtok(line, " ");
-            int idx = atoi(strtok(NULL, " "));
-
+        else if (strcmp(argv[i], "remove_block") == 0) {
+            i++;
+            CHECK_ARGC(i);  
+            int idx = atoi(argv[i]);
             real_time[0] = times(&tms_time[0]);
             delete_edit_block(manager, idx);
             real_time[1] = times(&tms_time[1]);
 
             print_time("delete_edit_block", real_time, tms_time);
+
+            i++;
         }
-
-        if (strstr(line, "remove_operation")) {
-            strtok(line, " ");
-            int block_idx = atoi(strtok(NULL, " "));
-            int op_idx = atoi(strtok(NULL, " "));
-
+        else if (strcmp(argv[i], "remove_operation") == 0) {
+            i += 2;
+            CHECK_ARGC(i);
+            
+            int block_idx = atoi(argv[i-1]);
+            int op_idx = atoi(argv[i]);
+            
             real_time[0] = times(&tms_time[0]);
             delete_operation(manager, block_idx, op_idx);
             real_time[1] = times(&tms_time[1]);
 
+            
             print_time("delete_operation", real_time, tms_time);
+            i++;
+        } else {
+            i++;
         }
     }
-
 
 
     return 0;
